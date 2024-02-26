d3e841da009bc59edca9072b80834bed6d3f3e2f
ふぁ <yuki@yuki0311.com>
2024-02-27 01:09:12 +0900

    change: 3.16.1でinitしていたので3.19.1でやり直し
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- android/app/src/main/kotlin/com/example/simple_github_search_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/simple_github_search_app/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.simple_github_search_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
