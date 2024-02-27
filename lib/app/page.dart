e57803d36412137136c005b703bbc7ad6e421736
ふぁ <yuki@yuki0311.com>
2024-02-28 02:27:04 +0900

    feat: 検索結果画面の実装 #23
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/app/page.dart
+++ lib/app/page.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:simple_github_search_app/component/repository_card.dart';
+import 'package:simple_github_search_app/infrastructure/github/model/param.dart';
+import 'package:simple_github_search_app/provider/github.dart';
 
 @RoutePage()
 class GithubSearchAppPage extends HookConsumerWidget {
@@ -8,9 +12,63 @@ class GithubSearchAppPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useScrollController();
+    final param = useState(const GithubSearchRepositoriesParam(q: 'flutter'));
+
+    useEffect(
+      () {
+        controller.addListener(() async {
+          if (controller.position.maxScrollExtent - 100 < controller.offset) {
+            final value = await ref.watch(githubSearchRepositoriesSearchProvider(param.value).future);
+            if (value.items.length != value.totalCount && context.mounted) {
+              await ref.read(githubSearchRepositoriesSearchProvider(param.value).notifier).nextPage();
+            }
+          }
+        });
+        return controller.dispose;
+      },
+      const [],
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GithubSearchApp'),
+      body: CustomScrollView(
+        controller: controller,
+        slivers: [
+          const SliverAppBar(
+            floating: true,
+            title: Text('Title'),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                final data = ref.watch(githubSearchRepositoriesSearchProvider(param.value)).requireValue;
+                return RepositoryCard(
+                  repository: data.items[index],
+                );
+              },
+              childCount: ref.watch(githubSearchRepositoriesSearchProvider(param.value)).maybeWhen(
+                    data: (response) => response.items.length,
+                    orElse: () => 0,
+                  ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ref.watch(githubSearchRepositoriesSearchProvider(param.value)).when(
+              data: (response) {
+                if (response.items.length != response.totalCount) {
+                  return const Padding(padding: EdgeInsets.all(8), child: Center(child: CircularProgressIndicator()));
+                }
+                return null;
+              },
+              loading: () {
+                return const Padding(padding: EdgeInsets.all(8), child: Center(child: CircularProgressIndicator()));
+              },
+              error: (Object error, StackTrace stackTrace) {
+                return Center(child: Text('Error: $error'));
+              },
+            ),
+          ),
+        ],
       ),
       drawer: const Drawer(),
       floatingActionButton: FloatingActionButton(
---
+++ lib/provider/github.g.dart
@@ -0,0 +1,336 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'github.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getGithubAPIClientHash() =>
+    r'281605d46a2da6b97e07412a824238dc5fe57aa9';
+
+/// See also [getGithubAPIClient].
+@ProviderFor(getGithubAPIClient)
+final getGithubAPIClientProvider = AutoDisposeProvider<GithubAPI>.internal(
+  getGithubAPIClient,
+  name: r'getGithubAPIClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getGithubAPIClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetGithubAPIClientRef = AutoDisposeProviderRef<GithubAPI>;
+String _$searchGithubRepositoriesRowHash() =>
+    r'9cd2e9e27de0bda0311bcb1e2ed4f25cae055f01';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [searchGithubRepositoriesRow].
+@ProviderFor(searchGithubRepositoriesRow)
+const searchGithubRepositoriesRowProvider = SearchGithubRepositoriesRowFamily();
+
+/// See also [searchGithubRepositoriesRow].
+class SearchGithubRepositoriesRowFamily
+    extends Family<AsyncValue<GithubResponse<GithubRepository>>> {
+  /// See also [searchGithubRepositoriesRow].
+  const SearchGithubRepositoriesRowFamily();
+
+  /// See also [searchGithubRepositoriesRow].
+  SearchGithubRepositoriesRowProvider call(
+    GithubSearchRepositoriesParam param,
+  ) {
+    return SearchGithubRepositoriesRowProvider(
+      param,
+    );
+  }
+
+  @override
+  SearchGithubRepositoriesRowProvider getProviderOverride(
+    covariant SearchGithubRepositoriesRowProvider provider,
+  ) {
+    return call(
+      provider.param,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'searchGithubRepositoriesRowProvider';
+}
+
+/// See also [searchGithubRepositoriesRow].
+class SearchGithubRepositoriesRowProvider
+    extends AutoDisposeFutureProvider<GithubResponse<GithubRepository>> {
+  /// See also [searchGithubRepositoriesRow].
+  SearchGithubRepositoriesRowProvider(
+    GithubSearchRepositoriesParam param,
+  ) : this._internal(
+          (ref) => searchGithubRepositoriesRow(
+            ref as SearchGithubRepositoriesRowRef,
+            param,
+          ),
+          from: searchGithubRepositoriesRowProvider,
+          name: r'searchGithubRepositoriesRowProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$searchGithubRepositoriesRowHash,
+          dependencies: SearchGithubRepositoriesRowFamily._dependencies,
+          allTransitiveDependencies:
+              SearchGithubRepositoriesRowFamily._allTransitiveDependencies,
+          param: param,
+        );
+
+  SearchGithubRepositoriesRowProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.param,
+  }) : super.internal();
+
+  final GithubSearchRepositoriesParam param;
+
+  @override
+  Override overrideWith(
+    FutureOr<GithubResponse<GithubRepository>> Function(
+            SearchGithubRepositoriesRowRef provider)
+        create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: SearchGithubRepositoriesRowProvider._internal(
+        (ref) => create(ref as SearchGithubRepositoriesRowRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        param: param,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeFutureProviderElement<GithubResponse<GithubRepository>>
+      createElement() {
+    return _SearchGithubRepositoriesRowProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SearchGithubRepositoriesRowProvider && other.param == param;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, param.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin SearchGithubRepositoriesRowRef
+    on AutoDisposeFutureProviderRef<GithubResponse<GithubRepository>> {
+  /// The parameter `param` of this provider.
+  GithubSearchRepositoriesParam get param;
+}
+
+class _SearchGithubRepositoriesRowProviderElement
+    extends AutoDisposeFutureProviderElement<GithubResponse<GithubRepository>>
+    with SearchGithubRepositoriesRowRef {
+  _SearchGithubRepositoriesRowProviderElement(super.provider);
+
+  @override
+  GithubSearchRepositoriesParam get param =>
+      (origin as SearchGithubRepositoriesRowProvider).param;
+}
+
+String _$githubSearchRepositoriesSearchHash() =>
+    r'8a26e731c72383837664794a3bb6f5579f3adba1';
+
+abstract class _$GithubSearchRepositoriesSearch
+    extends BuildlessAutoDisposeAsyncNotifier<
+        GithubResponse<GithubRepository>> {
+  late final GithubSearchRepositoriesParam param;
+
+  FutureOr<GithubResponse<GithubRepository>> build(
+    GithubSearchRepositoriesParam param,
+  );
+}
+
+/// See also [GithubSearchRepositoriesSearch].
+@ProviderFor(GithubSearchRepositoriesSearch)
+const githubSearchRepositoriesSearchProvider =
+    GithubSearchRepositoriesSearchFamily();
+
+/// See also [GithubSearchRepositoriesSearch].
+class GithubSearchRepositoriesSearchFamily
+    extends Family<AsyncValue<GithubResponse<GithubRepository>>> {
+  /// See also [GithubSearchRepositoriesSearch].
+  const GithubSearchRepositoriesSearchFamily();
+
+  /// See also [GithubSearchRepositoriesSearch].
+  GithubSearchRepositoriesSearchProvider call(
+    GithubSearchRepositoriesParam param,
+  ) {
+    return GithubSearchRepositoriesSearchProvider(
+      param,
+    );
+  }
+
+  @override
+  GithubSearchRepositoriesSearchProvider getProviderOverride(
+    covariant GithubSearchRepositoriesSearchProvider provider,
+  ) {
+    return call(
+      provider.param,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'githubSearchRepositoriesSearchProvider';
+}
+
+/// See also [GithubSearchRepositoriesSearch].
+class GithubSearchRepositoriesSearchProvider
+    extends AutoDisposeAsyncNotifierProviderImpl<GithubSearchRepositoriesSearch,
+        GithubResponse<GithubRepository>> {
+  /// See also [GithubSearchRepositoriesSearch].
+  GithubSearchRepositoriesSearchProvider(
+    GithubSearchRepositoriesParam param,
+  ) : this._internal(
+          () => GithubSearchRepositoriesSearch()..param = param,
+          from: githubSearchRepositoriesSearchProvider,
+          name: r'githubSearchRepositoriesSearchProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$githubSearchRepositoriesSearchHash,
+          dependencies: GithubSearchRepositoriesSearchFamily._dependencies,
+          allTransitiveDependencies:
+              GithubSearchRepositoriesSearchFamily._allTransitiveDependencies,
+          param: param,
+        );
+
+  GithubSearchRepositoriesSearchProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.param,
+  }) : super.internal();
+
+  final GithubSearchRepositoriesParam param;
+
+  @override
+  FutureOr<GithubResponse<GithubRepository>> runNotifierBuild(
+    covariant GithubSearchRepositoriesSearch notifier,
+  ) {
+    return notifier.build(
+      param,
+    );
+  }
+
+  @override
+  Override overrideWith(GithubSearchRepositoriesSearch Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: GithubSearchRepositoriesSearchProvider._internal(
+        () => create()..param = param,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        param: param,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeAsyncNotifierProviderElement<GithubSearchRepositoriesSearch,
+      GithubResponse<GithubRepository>> createElement() {
+    return _GithubSearchRepositoriesSearchProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is GithubSearchRepositoriesSearchProvider &&
+        other.param == param;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, param.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin GithubSearchRepositoriesSearchRef
+    on AutoDisposeAsyncNotifierProviderRef<GithubResponse<GithubRepository>> {
+  /// The parameter `param` of this provider.
+  GithubSearchRepositoriesParam get param;
+}
+
+class _GithubSearchRepositoriesSearchProviderElement
+    extends AutoDisposeAsyncNotifierProviderElement<
+        GithubSearchRepositoriesSearch, GithubResponse<GithubRepository>>
+    with GithubSearchRepositoriesSearchRef {
+  _GithubSearchRepositoriesSearchProviderElement(super.provider);
+
+  @override
+  GithubSearchRepositoriesParam get param =>
+      (origin as GithubSearchRepositoriesSearchProvider).param;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
