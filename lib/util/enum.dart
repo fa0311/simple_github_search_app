5cc6ca115c25d6e2e81363b6f0241871de631684
ふぁ <yuki@yuki0311.com>
2024-02-28 12:04:21 +0900

    feat: enum を安全に利用するためのツールを追加
    Signed-off-by: ふぁ <yuki@yuki0311.com>

---
+++ lib/util/enum.dart
@@ -0,0 +1,19 @@
+extension EnumByName<T extends Enum> on Iterable<T> {
+  T bySafeName(String name) {
+    for (final value in this) {
+      if (value.name == name) {
+        return value;
+      }
+    }
+    throw Exception('Not found');
+  }
+
+  T? byNameOrNull(String name) {
+    for (final value in this) {
+      if (value.name == name) {
+        return value;
+      }
+    }
+    return null;
+  }
+}
