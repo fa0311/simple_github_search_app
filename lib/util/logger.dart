0e91168f3f54a86d4a3124ec106346b4df1cdb0d
ふぁ <yuki@yuki0311.com>
2024-03-03 01:05:42 +0900

    feat: logger の設定を行う
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,3 +1,58 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-final logger = Logger();
+final logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 0,
+  ),
+);
+
+final dioLogger = PrettyDioLogger();
+
+class ProviderLogger extends ProviderObserver {
+  static bool common = false;
+
+  @override
+  void didAddProvider(
+    ProviderBase<dynamic> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    if (common) {
+      logger.t('provider: ${provider.name ?? provider.runtimeType}, value: $value');
+    }
+  }
+
+  @override
+  void providerDidFail(
+    ProviderBase<dynamic> provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    logger.w('provider: ${provider.name}', error: error, stackTrace: stackTrace);
+  }
+
+  @override
+  void didDisposeProvider(
+    ProviderBase<dynamic> provider,
+    ProviderContainer container,
+  ) {
+    if (common) {
+      logger.t('provider: ${provider.name ?? provider.runtimeType}, disposed');
+    }
+  }
+
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    if (common) {
+      logger.t('provider: ${provider.name ?? provider.runtimeType}, value: $newValue');
+    }
+  }
+}
