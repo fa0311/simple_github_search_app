b0fb2a43d6276db63d4719f81c3b94bea2b1d54f
ふぁ <yuki@yuki0311.com>
2024-03-04 05:32:06 +0900

    feat: test_util を追加
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- test/test_util/assets.dart
+++ test/test_util/assets.dart
@@ -4,6 +4,9 @@ import 'dart:io';
 class TestUtilAssets {
   TestUtilAssets._();
   static const basePath = 'test/assets';
+  static const iconPath = 'assets/icon/icon.png';
+
+  static const mockUrl = 'https://localhost:8080';
 
   static Future<Map<String, dynamic>> readJson(String path) async {
     final jsonString = await File('$basePath/$path').readAsString();
